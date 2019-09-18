a540fb2c93f073a162c914a41c0f6288e84fc24d
DoVanHao <DoVanHao@users.noreply.github.com>
2019-09-18 06:17:16 -0700

    Hàm hiển thị phiếu nhập

--- BusinessLogic/PhieuNhap.cs
+++ BusinessLogic/PhieuNhap.cs
@@ -64,33 +64,7 @@ namespace BusinessLogic
             return dt;
         }
 
-        public DataTable HT_CTPN(string ngay1, string ngay2)
-        {
-            string str = "HT_TK_CTPN";
-            SqlConnection con = new SqlConnection(KetNoiDB.getconnect());
-            DataTable dt = new DataTable();
-            SqlCommand cmd = new SqlCommand(str, con);
-            cmd.CommandType = CommandType.StoredProcedure;
-            cmd.Parameters.AddWithValue("@Ngay1", DateTime.Parse(ngay1));
-            cmd.Parameters.AddWithValue("@Ngay2", DateTime.Parse(ngay2));
-            SqlDataAdapter da = new SqlDataAdapter(cmd);
-            da.Fill(dt);
-            return dt;
-        }
-        //public DataTable PhieuNhap(string _MaNCC)
-        //{
-        //    string sql = "PhieuNhap";
-        //    DataTable dt = new DataTable();
-        //    SqlConnection con = new SqlConnection(KetNoiDB.getconnect());
-        //    con.Open();
-        //    SqlCommand cmd = new SqlCommand(sql, con);
-        //    cmd.CommandType = CommandType.StoredProcedure;
-
-        //    cmd.Parameters.AddWithValue("@mancc", _MaNCC);
-        //    SqlDataAdapter ad = new SqlDataAdapter(cmd);
-        //    ad.Fill(dt);
-        //    return dt;
-        //}
+ 
         public string InsertPN(string mancc, DateTime ngaynhap)
         {
             string sql = "ThemPN";
@@ -113,36 +87,6 @@ namespace BusinessLogic
             if (ma != null) return ma;
             return "error";
         }
-        //public void UpdateHDN(EC_HOADONNHAP et)
-        //{
-        //    string sql = "SuaHDN";
-        //    SqlConnection con = new SqlConnection(KetNoiDB.getconnect());
-        //    con.Open();
-        //    SqlCommand cmd = new SqlCommand(sql, con);
-        //    cmd.CommandType = CommandType.StoredProcedure;
-        //    cmd.Parameters.AddWithValue("@mahdn", et.MaHDN);
-        //    cmd.Parameters.AddWithValue("@mancc", et.MaNCC);
-        //    cmd.Parameters.AddWithValue("@ngaynhap", et.NgayNhap);
-
-
-        //    cmd.ExecuteNonQuery();
-        //    cmd.Dispose();
-        //    con.Close();
-
-        //}
-        //public void DeleteHDN(string _MaHDN)
-        //{
-        //    string sql = "XoaHDN";
-        //    SqlConnection con = new SqlConnection(KetNoiDB.getconnect());
-        //    con.Open();
-        //    SqlCommand cmd = new SqlCommand(sql, con);
-        //    cmd.CommandType = CommandType.StoredProcedure;
-        //    cmd.Parameters.AddWithValue("@mahdn", _MaHDN);
-
-        //    cmd.ExecuteNonQuery();
-        //    cmd.Dispose();
-        //    con.Close();
-        //}
 
         public string TongTien(string mapn)
         {
@@ -159,6 +103,31 @@ namespace BusinessLogic
             con.Close();
             string tien = dt.Rows[0].ItemArray[0].ToString();
             return tien;
+        }
+
+        public DataTable HienThiPN(string DieuKien)
+        {
+            string sql = @"SELECT MaPN, MaNCC, NgayNhap FROM PHIEUNHAP " + DieuKien;
+            DataTable dt = new DataTable();
+            SqlConnection con = new SqlConnection(KetNoiDB.getconnect());
+            SqlDataAdapter ad = new SqlDataAdapter(sql, con);
+            ad.Fill(dt);
+            return dt;
+        }
+        public DataTable HienThiPN(DateTime _NgayDau, DateTime _NgayCuoi)
+        {
+            string sql = "ThongKeHDN";
+            DataTable dt = new DataTable();
+            SqlConnection con = new SqlConnection(KetNoiDB.getconnect());
+            con.Open();
+            SqlCommand cmd = new SqlCommand(sql, con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@ngaydau", _NgayDau);
+            cmd.Parameters.AddWithValue("@ngaycuoi", _NgayCuoi);
+            SqlDataAdapter ad = new SqlDataAdapter(cmd);
+            ad.Fill(dt);
+            return dt;
         }
     }
 }
